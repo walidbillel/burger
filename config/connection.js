bd3fcb18c666815a837ca85104937340a347cd19
willou <walid.boubguira@gmail.com>
2018-08-30 21:16:32 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -9,16 +9,23 @@ var connection = mysql.createConnection({
   database: "qj46e5t6v44be2bb"
 });
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "burger_db"
-// });
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+}
+else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+}
+
 
 // Make connection.root
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
